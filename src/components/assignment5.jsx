e5c748a59d7e73966e6af458f3c2d249b61b524e
akash <akashiiit2001+1@gmail.com>
2024-04-22 16:08:01 +0530

    adding component for git username input

--- src/components/assignment5.jsx
+++ src/components/assignment5.jsx
@@ -1,3 +1,53 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 export default function Assignment5(){
+ useEffect(()=>{
+    axios.get("https://api.github.com/users/dreamsoffcode")
+ }, [])
     
+ 
+ 
+ return (
+    <div style={{
+        height: '92.7vh',
+        // flexDirection: 'column',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }}>
+        <InputUsername />
+    </div>
+ )
+}
+
+function InputUsername(){
+    return (
+        <div style={{
+            display: "flex",
+            width: "70vw",
+            justifyContent: 'center',
+        }}>
+            <input style={{
+                flexGrow: 1,
+                marginLeft: '3vw',
+                marginRight: '2vw',
+                padding: '5px',
+                fontSize: "22px",
+            }}
+            placeholder="Enter your github username"
+            />
+            <button style={{
+                backgroundColor: "black",
+                color: "white",
+                border: "none",
+                borderRadius: "5px",
+                fontSize: "22px",
+                padding: "12px 30px",
+                cursor: "pointer",
+            }}>
+                Submit
+            </button>
+        </div>
+    )
 }
