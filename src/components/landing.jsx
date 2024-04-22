2f159f94f73c09fdf75954d60c60e7c5e0467bbc
akash <akashiiit2001+1@gmail.com>
2024-04-22 12:38:01 +0530

    setting landing page footer

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,26 +1,23 @@
 export default function Landing() {
     return (
-        <div style={{height: "100%"}}>
-            <div>
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            height: "92.7vh"}}>
+            <div style={{
+                justifySelf: 'flex-start',
+                flexGrow: 1,
+            }}>
                 <h1>Hi there!</h1>
                 <h3>Select an option from the navigation bar go to to that specific component</h3>
             </div>
 
-            <footer style={{
-                display: 'flex',
-                flexDirection: 'column',
-                flexGrow: 1,
+            <div style={{
+                justifySelf: 'flex-end',
+                alignSelf: 'center'
             }}>
-                <div style={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignSelf: 'flex-end',
-                    flexGrow: 1,
-                    flexShrink: 0,
-                }}>
-                    Footer
-                </div>
-            </footer>
+                Developed by Akash Anand
+            </div>
 
 
 
